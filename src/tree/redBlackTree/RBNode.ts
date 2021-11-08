2e412fac93c89910e2a9bc2c3adaa9968c6c17bc
hoon11 <jhoonen@gmail.com>
2021-11-08 09:18:40 +0900

    Add RedBlackTree Node

---
+++ src/tree/redBlackTree/RBNode.ts
@@ -0,0 +1,120 @@
+/**
+ * 赤黒木のためなノードを定義したクラス
+ */
+export class RBNode<K, V> {
+  /**
+   * ノードの色
+   * false : 赤
+   * true : 黒
+   * @internal
+   */
+  private _color: boolean;
+
+  /**
+   * キー
+   * @internal
+   */
+  private _key: K;
+
+  /**
+   * 値
+   * @internal
+   */
+  private _value: V;
+
+  /**
+   * 左側の子
+   * @internal
+   */
+  private _leftChild: RBNode<K, V>;
+
+  /**
+   * 右側の子
+   * @internal
+   */
+  private _rightChild: RBNode<K, V>;
+
+  /**
+   * 親
+   * @internal
+   */
+  private _parentChild: RBNode<K, V>;
+
+  /**
+   * 赤色のノードを生成します。
+   *
+   * @param key - ノードのキー
+   * @param value - ノードの値
+   */
+  constructor(key: K, value: V) {
+    this._key = key;
+    this._value = value;
+    this._color = false;
+  }
+
+  /**
+   * ノードの色を判定します。
+   * 赤色ならfalse, 黒色ならtrueを返します。
+   *
+   * @returns boolean
+   */
+  public isRedColor(): boolean {
+    return this._color;
+  }
+
+  /**
+   * ノードを赤色にします。
+   *
+   * @returns void
+   */
+  public toRedColor(): void {
+    this._color = false;
+  }
+
+  /**
+   * ノードを黒色にします。
+   *
+   * @returns void
+   */
+  public toBlackColor(): void {
+    this._color = true;
+  }
+
+  get key(): K {
+    return this._key;
+  }
+
+  set key(key: K) {
+    this._key = key;
+  }
+
+  get value(): V {
+    return this._value;
+  }
+  set value(value: V) {
+    this._value = value;
+  }
+
+  get leftChild(): RBNode<K, V> {
+    return this._leftChild;
+  }
+  set leftChild(rbnode: RBNode<K, V>) {
+    this._leftChild = rbnode;
+  }
+
+  get rightChild(): RBNode<K, V> {
+    return this._rightChild;
+  }
+
+  set rightChild(rbnode: RBNode<K, V>) {
+    this._rightChild = rbnode;
+  }
+
+  get parentChild(): RBNode<K, V> {
+    return this._parentChild;
+  }
+
+  set parentChild(rbnode: RBNode<K, V>) {
+    this._parentChild = rbnode;
+  }
+}
